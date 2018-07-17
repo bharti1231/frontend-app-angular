728cf1e56314683961f0e3d74fa85be0d1d0cee7
kratikasaxena1021 <kratikasaxena0613@gmail.com>
2018-07-17 13:02:23 +0530

    first commit

---
+++ src/app/users/user.ts
@@ -0,0 +1,9 @@
+
+export class User {
+  public id: number;
+  public name: string;
+  public email: string;
+  public contact_number: string;
+  public address: string;
+  public gender: string;
+}
